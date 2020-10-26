cd4c2047be9106fe0bbbb7c87be040296b7c6beb
agilg00 <alum.agilg@iesalixar.org>
2020-10-26 18:14:57 +0100

    Ejercicios Tarea 8

---
+++ Tema 2 - Java/Tarea8/src/com/agilg00/Ej11.java
@@ -0,0 +1,20 @@
+package com.agilg00;
+
+import java.util.Scanner;
+
+public class Ej11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int num=1;
+
+
+        for (int i = 0; i <=10 ; i++) {
+
+            System.out.println(num+"x"+i+"="+i*num);
+
+
+        }
+    }
+}
