b302c530be769b3a22924189fca74310023e13ec
agilg00 <alum.agilg@iesalixar.org>
2020-10-26 16:29:35 +0100

    Ejercicios Tarea 8 Completados

---
+++ Tema 2 - Java/Tarea8/src/com/agilg00/Ej14.java
@@ -0,0 +1,27 @@
+package com.agilg00;
+
+import java.util.Scanner;
+
+public class Ej14 {
+
+    public static void main(String[] args) {
+        int altura;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la altura del triángulo");
+        altura = sc.nextInt();
+
+        for (int i = 0; i <=altura ; i++) {
+            for (int j = 0; j <= altura; j++) {
+                if (i+j <=altura ){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            
+        }
+    }
+}
